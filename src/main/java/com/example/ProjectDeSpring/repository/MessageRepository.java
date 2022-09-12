736e1b2d2c982488fcffe66b85019fd394115081
Karimfalll <mahamatfall@gmail.com>
2022-09-13 01:17:30 +0200

    Structuration 2

--- src/main/java/com/example/ProjectDeSpring/repository/MessageRepository.java
+++ src/main/java/com/example/ProjectDeSpring/repository/MessageRepository.java
@@ -1,8 +1,10 @@
 package com.example.ProjectDeSpring.repository;
 
+import com.example.ProjectDeSpring.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Repository
-public interface MessageRepository {
+public interface MessageRepository extends JpaRepository<Message, Integer> {
 }
