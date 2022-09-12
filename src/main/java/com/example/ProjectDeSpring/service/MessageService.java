46879860cde93a3252a16135a93127d00aa808e9
Karimfalll <mahamatfall@gmail.com>
2022-09-13 01:17:30 +0200

    Structuration 2

--- src/main/java/com/example/ProjectDeSpring/service/MessageService.java
+++ src/main/java/com/example/ProjectDeSpring/service/MessageService.java
@@ -1,7 +1,23 @@
 package com.example.ProjectDeSpring.service;
 
+import com.example.ProjectDeSpring.entity.Message;
+import com.example.ProjectDeSpring.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MessageService {
+
+    @Autowired
+    MessageRepository repository;
+
+    /*recup tous les message dans la base avec le methode findall definie dans jpa */
+
+    public List<Message> getAll(){
+        return repository.findAll();
+    }
+
 }
+
