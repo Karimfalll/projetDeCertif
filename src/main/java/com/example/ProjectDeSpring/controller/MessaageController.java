a9329ee7daa7125d8b6e121c6fc754d3f961a22f
Karimfalll <mahamatfall@gmail.com>
2022-09-13 01:38:20 +0200

    Modules d'affichage de message , dans controlleur,Rep,sercive

--- src/main/java/com/example/ProjectDeSpring/controller/MessaageController.java
+++ src/main/java/com/example/ProjectDeSpring/controller/MessaageController.java
@@ -1,7 +1,27 @@
 package com.example.ProjectDeSpring.controller;
 
+import com.example.ProjectDeSpring.entity.Message;
+import com.example.ProjectDeSpring.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/Api/Message")
 public class MessaageController {
+    @Autowired
+    MessageService service;
+
+    @GetMapping("/allmessages")
+
+    public List<Message> getAllMessaages(){
+
+        return service.getAll();
+
+    }
+
+
 }
