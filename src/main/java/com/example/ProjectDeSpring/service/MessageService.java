d735eac0d13f874970f2e9ce030aec2f2e5b91cf
Karimfalll <mahamatfall@gmail.com>
2022-09-13 14:54:44 +0200

    Karim - CREATION DES TOUTE LES METHODE CREUD POUR LE MODULE MESSAGE

--- src/main/java/com/example/ProjectDeSpring/service/MessageService.java
+++ src/main/java/com/example/ProjectDeSpring/service/MessageService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MessageService {
@@ -13,11 +14,47 @@ public class MessageService {
     @Autowired
     MessageRepository repository;
 
-    /*recup tous les message dans la base avec le methode findall definie dans jpa */
 
-    public List<Message> getAll(){
-        return repository.findAll();
+    //inserer un message dans la bd avec save predifinie dans JPA
+    public Message saveMessage(Message m){
+
+        try {
+            if (m.getContent().isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception + "the message can't be empty");
+        }
+        return repository.save(m);
     }
+
+
+
+    //Obtenir la liste de message du canal
+
+    public List<Message> getChannelMessages(long idChannal) {
+        return repository.getChannelMessage(idChannal);
+    }
+
+    //Ajouter ou Modifier un canal
+
+    public Message updateMessage(Message m) {
+
+
+        Message newMessage = repository.findById(Math.toIntExact((m.getId()))).orElse(null);
+
+        newMessage.setContent(m.getContent());
+        newMessage.setDateDepublication(m.getDateDepublication());
+        newMessage.setIdChannel(m.getIdChannel());
+        newMessage.setIdUser(m.getIdUser());
+
+        return saveMessage(newMessage);
+
+    }
+
+
+
 
 }
 
