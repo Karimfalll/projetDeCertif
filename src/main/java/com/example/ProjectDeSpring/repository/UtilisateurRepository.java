4ce9718672e73057fe4bea96382d48e4bc65cee8
Olivier Pichon de Vendeuil <108340085+Vendeuil@users.noreply.github.com>
2022-09-13 12:01:48 +0200

    Update UtilisateurRepository.java
    Je me suis inspiré de MessageRepository

--- src/main/java/com/example/ProjectDeSpring/repository/UtilisateurRepository.java
+++ src/main/java/com/example/ProjectDeSpring/repository/UtilisateurRepository.java
@@ -1,7 +1,10 @@
 package com.example.ProjectDeSpring.repository;
 
+import com.example.ProjectDeSpring.entity.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface UtilisateurRepository {
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
 }
