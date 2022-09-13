96202cb58a9fb747485329d90712ac999838c377
Karimfalll <mahamatfall@gmail.com>
2022-09-13 15:21:05 +0200

    Karim - Module message Service

--- src/main/java/com/example/ProjectDeSpring/repository/MessageRepository.java
+++ src/main/java/com/example/ProjectDeSpring/repository/MessageRepository.java
@@ -5,17 +5,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 
 
     @Query("SELECT m FROM Message m WHERE m.idChannel =:idChannel")
+    public  List<Message> getChannelMessages(@Param("idChannel")  long id);
 
-    public  List<Message> getChannelMessage(@Param("idChannel")  long id);
+
+    @Query("SELECT m FROM Message m WHERE m.idUser =:idUser")
+    public  List<Message> getUserMessages(@Param("idUser")  long id);
+
 
 }
